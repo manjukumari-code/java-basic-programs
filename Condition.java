0d0c806439c806c1dc5898e35533e28401c13e67
Manju Kumari <kumarimanju9251@gmail.com>
2025-12-29 14:05:33 +0530

    Create Condition.java

---
+++ Condition.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class condition
+{
+public static void main()
+{
+Scanner in=new Scanner(System.in);
+int n;
+System.out.print("enter the no=");
+n=in.nextInt();
+if (n%2==0)
+{
+System.out.println("Number is Even");
+}
+else
+{
+System.out.println("Number is odd");
+}
+}
+}
